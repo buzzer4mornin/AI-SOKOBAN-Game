f2c214c67d1321109a379f3c7de53f0e3cb2687c
ahmadli <aydin.ahmadli@merck.com>
2019-12-14 22:02:19 +0100

    Levels added

--- Sokoban4J-Playground/src/main/java/RunMyAgent.java
+++ Sokoban4J-Playground/src/main/java/RunMyAgent.java
@@ -5,9 +5,9 @@ class RunMyAgent {
     public static void main(String[] args) {
 		SokobanResult result;
 		// VISUALIZED GAME
-		//result = Sokoban.playAgentLevel("Easy/easy.sok",1, new MyAgent());
-		//result = Sokoban.playAgentLevel("sokobano.de/Aymeric_Medium.sok", 8, new MyAgent());
-		result = Sokoban.playAgentLevel("sokobano.de/Aymeric_Hard.sok", 10, new MyAgent());
+		//result = Sokoban.playAgentLevel("Easy/easy.sok",1, new MyAgent()); //There are 1-10 easy levels
+		//result = Sokoban.playAgentLevel("sokobano.de/Aymeric_Medium.sok", 8, new MyAgent()); //There are 1-10 medium levels
+		result = Sokoban.playAgentLevel("sokobano.de/Aymeric_Hard.sok", 1, new MyAgent()); //There are 1-20 hard levels
 		System.out.println("MyAgent result: " + result.getResult());
 		System.exit(0);
 	}
